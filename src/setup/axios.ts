2be2f67e55c03326e2aeb0607753a9705b00c3fc
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-12 21:11:54 +0700

    update authorization v1

--- src/setup/axios.ts
+++ src/setup/axios.ts
@@ -10,7 +10,8 @@ const instance = axios.create({
 instance.defaults.withCredentials = true
 
 // Alter defaults after instance has been created
-instance.defaults.headers.common['Authorization'] = `Bearer ${GET_JWT_LOCAL_STORAGE}`
+// instance.defaults.headers.common['Authorization'] = `Bearer ${GET_JWT_LOCAL_STORAGE}`
+instance.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('jwt')}`
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
