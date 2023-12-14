931466bff15421d9f73f13f98f329da1864293a5
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-15 02:18:50 +0700

    Updated View admin/users

--- src/component/ManageUser/Users.tsx
+++ src/component/ManageUser/Users.tsx
@@ -1,13 +1,15 @@
 "use client"
-import { useCallback, useEffect, useState } from "react"
+import { useCallback, useContext, useEffect, useState } from "react"
 import ReactPaginate from "react-paginate"
 import ModalDelete from "./ModalDelete"
 import ModalUser from "./ModalUser"
 import { deleteUser, fetchAllUser } from "@/services/userService"
 import { toast } from "react-toastify"
 import Link from "next/link"
+import { UserContext } from "@/context/UserContext"
 
 const Users = () => {
+    const { user } = useContext(UserContext)
     const [userLists, setUserList] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
     const [currentLimit, setCurrentLimit] = useState(3)
@@ -54,6 +56,7 @@ const Users = () => {
             setIsShowModalDelete(false)
         } else {
             toast.error(response.EM)
+            setIsShowModalDelete(false)
         }
     }
 
@@ -70,147 +73,163 @@ const Users = () => {
     }
     return (
         <>
-            <div className="manage-users-container">
-                <div className="user-header mt-2">
-                    <div className="title">
-                        <h3>Manage User</h3>
-                    </div>
-                    <div className="actions">
-                        <button
-                            className="btn btn-success me-2"
-                            onClick={() => window.location.reload()}
-                        >
-                            <i className="fa fa-refresh me-2" />
-                            Refesh
-                        </button>
-                        <button
-                            className="btn btn-primary"
-                            onClick={() => {
-                                setIsShowModalUser(true);
-                                setActionModalUser('CREATE');
-                            }}
-                        >
-                            <i className="fa fa-plus me-2" />
-                            Add new user
-                        </button>
-                    </div>
-                </div>
-                <div className="user-body">
-                    <div className="d-none d-sm-block">
-                        <table className="table table-hover table-bordered mt-3">
-                            <thead>
-                                <tr className="text-center table-dark">
-                                    <th colSpan={7}>USERS LIST</th>
-                                </tr>
-                                <tr>
-                                    <th scope="col">No</th>
-                                    <th scope="col">Id</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Phone</th>
-                                    <th scope="col">UserName</th>
-                                    <th scope="col">Group Role</th>
-                                    <th className="text-center">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {userLists && userLists.length > 0
-                                    ? userLists.map((userList: any, index: number) =>
-                                        <tr key={`row-${index}`}>
-                                            <th>{(currentPage - 1) * currentLimit + index + 1}</th>
-                                            <th>{userList.id}</th>
-                                            <td>{userList.email}</td>
-                                            <td>{userList.phone}</td>
-                                            <td>{userList.username}</td>
-                                            <td>{userList.Group ? userList.Group.name : null}</td>
-                                            <td className="text-center w-25">
-                                                <button
-                                                    className="btn btn-outline-warning me-2"
-                                                    type="submit"
-                                                    onClick={() => handleEditUser(userList)}
-                                                >
-                                                    <i className="fa fa-pen me-2" />
-                                                    Edit
-                                                </button>
-                                                <button
-                                                    className="btn btn-outline-danger"
-                                                    type="submit"
-                                                    onClick={() => hanldeDeleteUser(userList)}
-                                                >
-                                                    <i className="fa fa-trash me-2" />
-                                                    Delete
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    )
-                                    : <tr>
-                                        <td>Not found users</td>
-                                    </tr>
-                                }
-                            </tbody>
-                        </table>
-                    </div>
-
-                    {/* Reponsive mobile */}
-                    <div className="card mb-3 d-block d-sm-none mt-4" style={{ maxWidth: '540px' }}>
-                        <div className="row g-0">
-                            <div className="col-md-8">
-                                {userLists && userLists.length > 0 &&
-                                    userLists.map((item: any, index: number) => {
-                                        return (
-                                            <div className="card-body" key={index}>
-                                                <p className="card-title text-primary fw-bold">No: {index + 1}</p>
-                                                <p className="card-text"><span className="fw-bold">Id:</span> {item.id} </p>
-                                                <p className="card-text"><span className="fw-bold">Email:</span> {item.email} </p>
-                                                <p className="card-text"><span className="fw-bold">Phone:</span> {item.phone} </p>
-                                                <p className="card-text"><span className="fw-bold">UserName:</span> {item.username} </p>
-                                                <p className="card-text"><span className="fw-bold">Group Role:</span> {item.Group ? item.Group.name : null} </p>
-                                                <div className="action">
-                                                    <p className="card-text fw-bold">Actions: </p>
-                                                    <Link href="" 
-                                                        onClick={() => handleEditUser(item)}
-                                                        className="btn btn-warning d-block d-sm-inline"
-                                                    >Edit</Link>
-                                                    <Link href="" 
-                                                        onClick={() => hanldeDeleteUser(item)}
-                                                        className="btn btn-danger mx-0 my-2 mx-sm-2 d-block d-sm-inline"
-                                                    >Delete</Link>
-                                                </div>
-                                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                                                <hr />
-                                            </div>
-                                        )
-                                    })
-                                }
-                            </div>
+            {user.isAuthenticated && userLists && userLists.length > 0
+                ? <div className="manage-users-container">
+                    <div className="user-header mt-2">
+                        <div className="title">
+                            <h3>Manage User</h3>
+                        </div>
+                        <div className="actions">
+                            <button
+                                className="btn btn-success me-2"
+                                onClick={() => window.location.reload()}
+                            >
+                                <i className="fa fa-refresh me-2" />
+                                Refesh
+                            </button>
+                            <button
+                                className="btn btn-primary"
+                                onClick={() => {
+                                    setIsShowModalUser(true);
+                                    setActionModalUser('CREATE');
+                                }}
+                            >
+                                <i className="fa fa-plus me-2" />
+                                Add new user
+                            </button>
                         </div>
                     </div>
-                    {/* ----->>>>>>>>>>>------- */}
-                </div>
-                {totalPages > 0 &&
-                    <div className="user-footer">
-                        <ReactPaginate
-                            nextLabel="next >"
-                            onPageChange={handlePageClick}
-                            pageRangeDisplayed={3}
-                            marginPagesDisplayed={4}
-                            pageCount={totalPages}
-                            previousLabel="< previous"
-                            pageClassName="page-item"
-                            pageLinkClassName="page-link"
-                            previousClassName="page-item"
-                            previousLinkClassName="page-link"
-                            nextClassName="page-item"
-                            nextLinkClassName="page-link"
-                            breakLabel="..."
-                            breakClassName="page-item"
-                            breakLinkClassName="page-link"
-                            containerClassName="pagination"
-                            activeClassName="active"
-                            renderOnZeroPageCount={null}
-                        />
+                    <div className="user-body">
+                        <div className="d-none d-sm-block">
+                            <table className="table table-hover table-bordered mt-3">
+                                <thead>
+                                    <tr className="text-center table-dark">
+                                        <th colSpan={7}>USERS LIST</th>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col">No</th>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Phone</th>
+                                        <th scope="col">UserName</th>
+                                        <th scope="col">Group Role</th>
+                                        <th className="text-center">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {userLists && userLists.length > 0
+                                        ? userLists.map((userList: any, index: number) =>
+                                            <tr key={`row-${index}`}>
+                                                <th>{(currentPage - 1) * currentLimit + index + 1}</th>
+                                                <th>{userList.id}</th>
+                                                <td>{userList.email}</td>
+                                                <td>{userList.phone}</td>
+                                                <td>{userList.username}</td>
+                                                <td>{userList.Group ? userList.Group.name : null}</td>
+                                                {
+                                                    !(userList.email === user.account.email)
+                                                        ?
+                                                        <td className="text-center w-25">
+                                                            <button
+                                                                className="btn btn-outline-warning me-2"
+                                                                type="submit"
+                                                                onClick={() => handleEditUser(userList)}
+                                                            >
+                                                                <i className="fa fa-pen me-2" />
+                                                                Edit
+                                                            </button>
+                                                            <button
+                                                                className="btn btn-outline-danger"
+                                                                type="submit"
+                                                                onClick={() => hanldeDeleteUser(userList)}
+                                                            >
+                                                                <i className="fa fa-trash me-2" />
+                                                                Delete
+                                                            </button>
+                                                        </td>
+                                                        :
+                                                        <td><div className="alert alert-primary text-center m-0 p-0">no action</div></td>
+                                                }
+                                            </tr>
+                                        )
+                                        : <tr>
+                                            <td>Not found users</td>
+                                        </tr>
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+
+                        {/* Reponsive mobile */}
+                        <div className="card mb-3 d-block d-sm-none mt-4" style={{ maxWidth: '540px' }}>
+                            <div className="row g-0">
+                                <div className="col-md-8">
+                                    {userLists && userLists.length > 0 &&
+                                        userLists.map((item: any, index: number) => {
+                                            return (
+                                                <div className="card-body" key={index}>
+                                                    <p className="card-title text-primary fw-bold">No: {index + 1}</p>
+                                                    <p className="card-text"><span className="fw-bold">Id:</span> {item.id} </p>
+                                                    <p className="card-text"><span className="fw-bold">Email:</span> {item.email} </p>
+                                                    <p className="card-text"><span className="fw-bold">Phone:</span> {item.phone} </p>
+                                                    <p className="card-text"><span className="fw-bold">UserName:</span> {item.username} </p>
+                                                    <p className="card-text"><span className="fw-bold">Group Role:</span> {item.Group ? item.Group.name : null} </p>
+                                                    {
+                                                        !(item.email === user.account.email)
+                                                            ?
+                                                            <div className="action">
+                                                                <p className="card-text fw-bold">Actions: </p>
+                                                                <Link href=""
+                                                                    onClick={() => handleEditUser(item)}
+                                                                    className="btn btn-warning d-block d-sm-inline"
+                                                                >Edit</Link>
+                                                                <Link href=""
+                                                                    onClick={() => hanldeDeleteUser(item)}
+                                                                    className="btn btn-danger mx-0 my-2 mx-sm-2 d-block d-sm-inline"
+                                                                >Delete</Link>
+                                                            </div>
+                                                            : <div className="alert alert-primary text-center m-0 p-0">no action</div>
+                                                    }
+
+                                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                                    <hr />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        {/* ----->>>>>>>>>>>------- */}
                     </div>
-                }
-            </div>
+                    {totalPages > 0 &&
+                        <div className="user-footer">
+                            <ReactPaginate
+                                nextLabel="next >"
+                                onPageChange={handlePageClick}
+                                pageRangeDisplayed={3}
+                                marginPagesDisplayed={4}
+                                pageCount={totalPages}
+                                previousLabel="< previous"
+                                pageClassName="page-item"
+                                pageLinkClassName="page-link"
+                                previousClassName="page-item"
+                                previousLinkClassName="page-link"
+                                nextClassName="page-item"
+                                nextLinkClassName="page-link"
+                                breakLabel="..."
+                                breakClassName="page-item"
+                                breakLinkClassName="page-link"
+                                containerClassName="pagination"
+                                activeClassName="active"
+                                renderOnZeroPageCount={null}
+                            />
+                        </div>
+                    }
+                </div>
+                : <div className="alert alert-primary text-center">No Data...!</div>
+            }
+
             <ModalDelete
                 show={isShowModalDelete}
                 dataModal={dataModal}
