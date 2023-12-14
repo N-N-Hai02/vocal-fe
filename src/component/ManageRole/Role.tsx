75bf7c16be08a808f4e31c0facd1c1ec4232d4c8
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-15 03:06:44 +0700

    Updated view admin/roles

--- src/component/ManageRole/Role.tsx
+++ src/component/ManageRole/Role.tsx
@@ -1,11 +1,13 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useContext } from 'react'
 import _ from 'lodash'
 import { v4 as uuidv4 } from 'uuid';
 import { toast } from 'react-toastify';
 import TableRole from './TableRole';
 import { createNewRoles } from '@/services/roleService';
+import { UserContext } from '@/context/UserContext';
 
 const Role = () => {
+    const { user } = useContext(UserContext)
     const dataChildDefault = { url: '', description: '', isValidUrl: true }
     const [listChild, setListChild] = useState({
         child_1: dataChildDefault
@@ -67,50 +69,55 @@ const Role = () => {
     }
 
     return (
-        <div className='role-container'>
-            <div className='title-role'><h4>Add a new role...</h4></div>
-            <div className='role-parent'>
-                {
-                    Object.entries(listChild).map(([key, child], index) => {
-                        return (
-                            <div className={`row role-child ${key}`} key={`child-${key}`}>
-                                <div className='col-12 col-sm-5 form-group'>
-                                    <label>URL:</label>
-                                    <input
-                                        type='text'
-                                        className={child.isValidUrl ? 'form-control' : 'form-control is-invalid'}
-                                        value={child.url}
-                                        onChange={(event) => handleOnchangeInput("url", event.target.value, key)}
-                                    />
-                                </div>
-                                <div className='col-12 col-sm-5 form-group'>
-                                    <label>Description:</label>
-                                    <input
-                                        type='text'
-                                        className='form-control'
-                                        value={child.description}
-                                        onChange={(event) => handleOnchangeInput("description", event.target.value, key)}
-                                    />
-                                </div>
-                                <div className='col-2 mt-4'>
-                                    {key && key === "child_1"
-                                        ?
-                                        <i className="fa fa-plus-circle text-success px-2 fs-2" onClick={() => handleAddNewRole()}></i>
-                                        :
-                                        <i className="fa fa-trash me-2 text-danger ps-2 fs-2" onClick={() => handleRemoveRole(key)} />
-                                    }
-                                </div>
-                            </div>
-                        )
-                    })}
-                <div className='role-save mt-2'>
-                    <button className='btn btn-warning px-5' onClick={() => handleSave()}>save</button>
+        <>
+            {user.isAuthenticated ?
+                <div className='role-container'>
+                    <div className='title-role'><h4>Add a new role...</h4></div>
+                    <div className='role-parent'>
+                        {
+                            Object.entries(listChild).map(([key, child], index) => {
+                                return (
+                                    <div className={`row role-child ${key}`} key={`child-${key}`}>
+                                        <div className='col-12 col-sm-5 form-group'>
+                                            <label>URL:</label>
+                                            <input
+                                                type='text'
+                                                className={child.isValidUrl ? 'form-control' : 'form-control is-invalid'}
+                                                value={child.url}
+                                                onChange={(event) => handleOnchangeInput("url", event.target.value, key)}
+                                            />
+                                        </div>
+                                        <div className='col-12 col-sm-5 form-group'>
+                                            <label>Description:</label>
+                                            <input
+                                                type='text'
+                                                className='form-control'
+                                                value={child.description}
+                                                onChange={(event) => handleOnchangeInput("description", event.target.value, key)}
+                                            />
+                                        </div>
+                                        <div className='col-2 mt-4'>
+                                            {key && key === "child_1"
+                                                ?
+                                                <i className="fa fa-plus-circle text-success px-2 fs-2" onClick={() => handleAddNewRole()}></i>
+                                                :
+                                                <i className="fa fa-trash me-2 text-danger ps-2 fs-2" onClick={() => handleRemoveRole(key)} />
+                                            }
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        <div className='role-save mt-2'>
+                            <button className='btn btn-warning px-5' onClick={() => handleSave()}>save</button>
+                        </div>
+                    </div>
+                    <div className='table-role mt-3'>
+                        <TableRole ref={childRef} user={user} />
+                    </div>
                 </div>
-            </div>
-            <div className='table-role mt-3'>
-                <TableRole ref={childRef} />
-            </div>
-        </div>
+                : <div className="alert alert-primary text-center">No Data...!</div>
+            }
+        </>
     )
 }
 
--- src/component/ManageRole/TableRole.tsx
+++ src/component/ManageRole/TableRole.tsx
@@ -5,7 +5,12 @@ import { toast } from "react-toastify";
 import ModalRole from "./ModalRole";
 import Link from "next/link";
 
-const TableRole = forwardRef((props, ref) => {
+interface TableRole {
+    user: any,
+    ref: () => void
+}
+
+const TableRole = forwardRef(({ user, ref }: TableRole) => {
     const [roleLists, setRoleLists] = useState([])
     const [isShow, setIsShow] = useState(false)
     const [dataModalRole, setDataModalRole] = useState({})
@@ -53,109 +58,114 @@ const TableRole = forwardRef((props, ref) => {
 
     return (
         <>
-            <div className="table-content">
-                <div className="d-none d-sm-block">
-                    <table className="table table-hover table-bordered mt-3">
-                        <thead>
-                            <tr className="text-center table-dark">
-                                <th colSpan={7}>ROLES LIST</th>
-                            </tr>
-                            <tr>
-                                <th scope="col">Id</th>
-                                <th scope="col">URL</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {roleLists && roleLists.length > 0
-                                ? roleLists.map((roleList: any, index: number) =>
-                                    <tr key={`row-${index}`}>
-                                        <th>{roleList.id}</th>
-                                        <td>{roleList.url}</td>
-                                        <td>{roleList.description}</td>
-                                        <td className="text-center w-25">
-                                            <button
-                                                className="btn btn-outline-warning me-2"
-                                                type="submit"
-                                                onClick={() => handleEditRole(roleList)}
-                                            >
-                                                <i className="fa fa-pen me-2" />
-                                                Edit
-                                            </button>
-                                            <button
-                                                className="btn btn-outline-danger"
-                                                type="submit"
-                                                onClick={() => hanldeDeleteRole(roleList)}
-                                            >
-                                                <i className="fa fa-trash me-2" />
-                                                Delete
-                                            </button>
-                                        </td>
+            {
+                user.isAuthenticated && roleLists && roleLists.length > 1
+                    ?
+                    <div className="table-content">
+                        <div className="d-none d-sm-block">
+                            <table className="table table-hover table-bordered mt-3">
+                                <thead>
+                                    <tr className="text-center table-dark">
+                                        <th colSpan={7}>ROLES LIST</th>
                                     </tr>
-                                )
-                                : <tr>
-                                    <td colSpan={4}>Not found roles</td>
-                                </tr>
-                            }
-                        </tbody>
-                    </table>
-                </div>
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">URL</th>
+                                        <th scope="col">Description</th>
+                                        <th scope="col">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {roleLists && roleLists.length > 0
+                                        ? roleLists.map((roleList: any, index: number) =>
+                                            <tr key={`row-${index}`}>
+                                                <th>{roleList.id}</th>
+                                                <td>{roleList.url}</td>
+                                                <td>{roleList.description}</td>
+                                                <td className="text-center w-25">
+                                                    <button
+                                                        className="btn btn-outline-warning me-2"
+                                                        type="submit"
+                                                        onClick={() => handleEditRole(roleList)}
+                                                    >
+                                                        <i className="fa fa-pen me-2" />
+                                                        Edit
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-outline-danger"
+                                                        type="submit"
+                                                        onClick={() => hanldeDeleteRole(roleList)}
+                                                    >
+                                                        <i className="fa fa-trash me-2" />
+                                                        Delete
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        )
+                                        : <tr>
+                                            <td colSpan={4}>Not found roles</td>
+                                        </tr>
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
 
-                {/* Reponsive mobile */}
-                <div className="card mb-3 d-block d-sm-none" style={{ maxWidth: '540px' }}>
-                    <div className="row g-0">
-                        <div className="col-md-8">
-                            {roleLists && roleLists.length > 0 &&
-                                roleLists.map((item: any, index: number) => {
-                                    return (
-                                        <div className="card-body" key={index}>
-                                            <p className="card-title text-primary fw-bold">No: {index + 1}</p>
-                                            <p className="card-text"><span className="fw-bold">Id:</span> {item.id} </p>
-                                            <p className="card-text"><span className="fw-bold">URL:</span> {item.url} </p>
-                                            <p className="card-text"><span className="fw-bold">Description:</span> {item.description} </p>
-                                            <div className="action">
-                                                <p className="card-text fw-bold">Action: </p>
-                                                <Link href="" className="btn btn-warning d-block d-sm-inline" onClick={() => handleEditRole(item)}>Edit</Link>
-                                                <Link href="" className="btn btn-danger mx-0 my-2 mx-sm-2 d-block d-sm-inline" onClick={() => hanldeDeleteRole(item)}>Delete</Link>
-                                            </div>
-                                            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                                            <hr />
-                                        </div>
-                                    )
-                                })
-                            }
+                        {/* Reponsive mobile */}
+                        <div className="card mb-3 d-block d-sm-none" style={{ maxWidth: '540px' }}>
+                            <div className="row g-0">
+                                <div className="col-md-8">
+                                    {roleLists && roleLists.length > 0 &&
+                                        roleLists.map((item: any, index: number) => {
+                                            return (
+                                                <div className="card-body" key={index}>
+                                                    <p className="card-title text-primary fw-bold">No: {index + 1}</p>
+                                                    <p className="card-text"><span className="fw-bold">Id:</span> {item.id} </p>
+                                                    <p className="card-text"><span className="fw-bold">URL:</span> {item.url} </p>
+                                                    <p className="card-text"><span className="fw-bold">Description:</span> {item.description} </p>
+                                                    <div className="action">
+                                                        <p className="card-text fw-bold">Action: </p>
+                                                        <Link href="" className="btn btn-warning d-block d-sm-inline" onClick={() => handleEditRole(item)}>Edit</Link>
+                                                        <Link href="" className="btn btn-danger mx-0 my-2 mx-sm-2 d-block d-sm-inline" onClick={() => hanldeDeleteRole(item)}>Delete</Link>
+                                                    </div>
+                                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                                    <hr />
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        </div>
+                        {/* ----->>>>>>>>>>>------- */}
+
+                        <div className="role-paginate">
+                            <ReactPaginate
+                                nextLabel="next >"
+                                onPageChange={handlePageClick}
+                                pageRangeDisplayed={3}
+                                marginPagesDisplayed={4}
+                                pageCount={2}
+                                previousLabel="< previous"
+                                pageClassName="page-item"
+                                pageLinkClassName="page-link"
+                                previousClassName="page-item"
+                                previousLinkClassName="page-link"
+                                nextClassName="page-item"
+                                nextLinkClassName="page-link"
+                                breakLabel="..."
+                                breakClassName="page-item"
+                                breakLinkClassName="page-link"
+                                containerClassName="pagination"
+                                activeClassName="active"
+                                renderOnZeroPageCount={null}
+                            />
                         </div>
                     </div>
-                </div>
-                {/* ----->>>>>>>>>>>------- */}
-
-                <div className="role-paginate">
-                    <ReactPaginate
-                        nextLabel="next >"
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={3}
-                        marginPagesDisplayed={4}
-                        pageCount={2}
-                        previousLabel="< previous"
-                        pageClassName="page-item"
-                        pageLinkClassName="page-link"
-                        previousClassName="page-item"
-                        previousLinkClassName="page-link"
-                        nextClassName="page-item"
-                        nextLinkClassName="page-link"
-                        breakLabel="..."
-                        breakClassName="page-item"
-                        breakLinkClassName="page-link"
-                        containerClassName="pagination"
-                        activeClassName="active"
-                        renderOnZeroPageCount={null}
-                    />
-                </div>
-            </div>
+                    : <div className="alert alert-primary text-center">No Data...!</div>
+            }
 
             <ModalRole
-                show={isShow} 
+                show={isShow}
                 onHide={handleCloseModal}
                 dataModalRole={dataModalRole}
             />
