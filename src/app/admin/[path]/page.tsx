46fc327b70fcec63a72c03f5ef6706981407067d
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-14 22:06:34 +0700

    check permisstion admin - path

--- src/app/admin/[path]/page.tsx
+++ src/app/admin/[path]/page.tsx
@@ -4,12 +4,17 @@ import Level from '@/component/ManageLevel/Level'
 import Role from '@/component/ManageRole/Role'
 import Users from '@/component/ManageUser/Users'
 import Vocal from '@/component/ManageVocal/Vocal'
+import { UserContext } from '@/context/UserContext'
 import { redirect, usePathname } from 'next/navigation'
+import { useContext } from 'react'
 
 const AppRoutes = () => {
     const pathname = usePathname()
+    const { user } = useContext(UserContext)
 
-    const authenticated = true
+    // check permission step 1
+    let authenticated:boolean
+    user.isLoading === false ? authenticated = true : authenticated = user.isAuthenticated && user.account.groupWithRoles.id === 2 && user.account.groupWithRoles.name === 'admin'
 
     if (authenticated) {
         switch (pathname) {
