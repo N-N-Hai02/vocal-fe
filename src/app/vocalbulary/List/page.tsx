efb4e2e5744d6f900ff14ac31667fa4341c12cc6
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-15 02:25:36 +0700

    Updated view vocalbulary/list

--- src/app/vocalbulary/List/page.tsx
+++ src/app/vocalbulary/List/page.tsx
@@ -15,14 +15,14 @@ export default function VocabularyList() {
 
     useEffect(() => setVlaue(1), [])
 
-    const handleStatus = async (data:any) => {
+    const handleStatus = async (data: any) => {
         let resultData = {
             userId: user.account.groupWithRoles.id,
             vocalId: data.id
         }
 
         if (data && confirm(`Assign: ${data.en} to ${user.account.username}`) == true) {
-            let res:any = await vocalAssignToUser(resultData)
+            let res: any = await vocalAssignToUser(resultData)
             if (res && res.EC === 0) {
                 toast.success(res.EM)
                 getAllVocalByUser()
@@ -37,11 +37,11 @@ export default function VocabularyList() {
     }, [])
 
     const getAllVocalByUser = async () => {
-        let res:any = await fechAllVocalByUser()
+        let res: any = await fechAllVocalByUser()
         res && res.EC === 0 && setVocalByUserList(res.DT)
     }
 
-    const vocalId = vocalByUserList.reduce((accumulator:any, currentValue:any) => {
+    const vocalId = vocalByUserList.reduce((accumulator: any, currentValue: any) => {
         if (currentValue.userId === user.account.groupWithRoles?.id) {
             accumulator.push(currentValue.vocalId)
         }
@@ -52,89 +52,94 @@ export default function VocabularyList() {
     
     return (
         <div className="h-100">
-            <div className="card rounded-0">
-                <h5 className="card-header text-uppercase">Danh sách từ</h5>
-                <div className="card-body">
-                    <div className="row input-group mb-3">
-                        <div className="col-12 col-sm-4">
-                            <label className="my-2 fw-bold text-warning">Level - english</label>
-                            <select
-                                className="form-select"
-                                aria-label="Default select example"
-                                onChange={(e) => setVlaue(+e.target.value)}
-                            >
-                                {levelVocal[0]?.map((item:any, index:number) => <option key={index} value={item.id}>{item.name}</option>)}
-                            </select>
-                        </div>
-                    </div>
-                    <div className="d-none d-sm-block">
-                        <table className="table table-hover text-center">
-                            <thead>
-                                <tr className="table-dark">
-                                    <th scope="col">No</th>
-                                    <th scope="col">English</th>
-                                    <th scope="col">Spelling</th>
-                                    <th scope="col">Pronunciation</th>
-                                    <th scope="col">Vietnamese</th>
-                                    <th scope="col">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {data.map((item: any, index:number) => (
-                                    <tr key={index}>
-                                        <td>{index + 1}</td>
-                                        <th scope="row" onClick={() => checkClickVocalbulary(index)}>
-                                            <Link href="/vocalbulary/Detail">{item.en}</Link>
-                                        </th>
-                                        <td>{item.spelling}</td>
-                                        <td>{item.pronunciation}</td>
-                                        <td>{item.vn}</td>
-                                        <td>
-                                            <button className="btn btn-outline-warning" onClick={() => handleStatus(item)}>
-                                                {
-                                                    vocalId && vocalId.includes(item.id)
-                                                    ? <i className="fas fa-star"></i>
-                                                    : <i className="far fa-star"></i>
-                                                }
-                                            </button>
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-
-                    {/* Reponsive mobile */}
-                    <div className="card mb-3 d-block d-sm-none" style={{ maxWidth: '540px' }}>
-                        <div className="row g-0">
-                            <div className="col-md-8">
-                                {data && data.length > 0 &&
-                                    data.map((item: any, index: number) => {
-                                        return (
-                                            <div className="card-body" key={index}>
-                                                <p className="card-title text-primary fw-bold">No: {index + 1}</p>
-                                                <p className="card-text"><span className="fw-bold">Spelling:</span> {item.en} </p>
-                                                <p className="card-text"><span className="fw-bold">English:</span> {item.vn} </p>
-                                                <p className="card-text"><span className="fw-bold">Vietnamese:</span> {item.spelling} </p>
-                                                <button className="btn btn-outline-warning" onClick={() => handleStatus(item)}>
-                                                    {
-                                                        vocalId && vocalId.includes(item.id)
-                                                        ? <i className="fas fa-star"></i>
-                                                        : <i className="far fa-star"></i>
-                                                    }
-                                                </button>
-                                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                                                <hr />
-                                            </div>
-                                        )
-                                    })
-                                }
+            {
+                user.isAuthenticated && data && data.length > 0 && levelVocal[0] && levelVocal[0]?.length > 0
+                    ?
+                    <div className="card rounded-0">
+                        <h5 className="card-header text-uppercase">Danh sách từ</h5>
+                        <div className="card-body">
+                            <div className="row input-group mb-3">
+                                <div className="col-12 col-sm-4">
+                                    <label className="my-2 fw-bold text-warning">Level - english</label>
+                                    <select
+                                        className="form-select"
+                                        aria-label="Default select example"
+                                        onChange={(e) => setVlaue(+e.target.value)}
+                                    >
+                                        {levelVocal[0]?.map((item: any, index: number) => <option key={index} value={item.id}>{item.name}</option>)}
+                                    </select>
+                                </div>
                             </div>
+                            <div className="d-none d-sm-block">
+                                <table className="table table-hover text-center">
+                                    <thead>
+                                        <tr className="table-dark">
+                                            <th scope="col">No</th>
+                                            <th scope="col">English</th>
+                                            <th scope="col">Spelling</th>
+                                            <th scope="col">Pronunciation</th>
+                                            <th scope="col">Vietnamese</th>
+                                            <th scope="col">Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {data.map((item: any, index: number) => (
+                                            <tr key={index}>
+                                                <td>{index + 1}</td>
+                                                <th scope="row" onClick={() => checkClickVocalbulary(index)}>
+                                                    <Link href="/vocalbulary/Detail">{item.en}</Link>
+                                                </th>
+                                                <td>{item.spelling}</td>
+                                                <td>{item.pronunciation}</td>
+                                                <td>{item.vn}</td>
+                                                <td>
+                                                    <button className="btn btn-outline-warning" onClick={() => handleStatus(item)}>
+                                                        {
+                                                            vocalId && vocalId.includes(item.id)
+                                                                ? <i className="fas fa-star"></i>
+                                                                : <i className="far fa-star"></i>
+                                                        }
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            {/* Reponsive mobile */}
+                            <div className="card mb-3 d-block d-sm-none" style={{ maxWidth: '540px' }}>
+                                <div className="row g-0">
+                                    <div className="col-md-8">
+                                        {data && data.length > 0 &&
+                                            data.map((item: any, index: number) => {
+                                                return (
+                                                    <div className="card-body" key={index}>
+                                                        <p className="card-title text-primary fw-bold">No: {index + 1}</p>
+                                                        <p className="card-text"><span className="fw-bold">Spelling:</span> {item.en} </p>
+                                                        <p className="card-text"><span className="fw-bold">English:</span> {item.vn} </p>
+                                                        <p className="card-text"><span className="fw-bold">Vietnamese:</span> {item.spelling} </p>
+                                                        <button className="btn btn-outline-warning" onClick={() => handleStatus(item)}>
+                                                            {
+                                                                vocalId && vocalId.includes(item.id)
+                                                                    ? <i className="fas fa-star"></i>
+                                                                    : <i className="far fa-star"></i>
+                                                            }
+                                                        </button>
+                                                        <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                                        <hr />
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                            {/* ----->>>>>>>>>>>------- */}
                         </div>
                     </div>
-                    {/* ----->>>>>>>>>>>------- */}
-                </div>
-            </div>
+                    : <div className="alert alert-primary text-center">No Data...!</div>
+            }
         </div>
     )
 }
