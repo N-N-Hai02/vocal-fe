047a5b7a60395704be3333e3b83b29ef1ec86dc6
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-15 06:01:20 +0700

    Updated view vocalbulary/Detail - <ReactPaginate />

--- src/component/app.paginate.tsx
+++ src/component/app.paginate.tsx
@@ -9,14 +9,14 @@ interface AppPaginate {
 const AppPaginate = ({totalPages, handlePageClick, pathName}: AppPaginate) => {
     return (
         <div className="app-pagination">
-            {pathName && pathName !== '#' && <Link href={pathName} className="btn btn-primary float-start me-2">Go back</Link>}
+            {pathName && pathName !== '#' && <Link href={pathName} className="btn btn-primary float-start me-2"><i className="fa fa-backward me-2"></i>Go back</Link>}
             <ReactPaginate
-                nextLabel="next >"
+                nextLabel={<i className="ms-2 fa fa-forward"></i>}
                 onPageChange={handlePageClick}
                 pageRangeDisplayed={1}
                 marginPagesDisplayed={0}
                 pageCount={totalPages}
-                previousLabel="< previous"
+                previousLabel={<i className="fa fa-backward"></i>}
                 pageClassName="page-item"
                 pageLinkClassName="page-link"
                 previousClassName="page-item"
