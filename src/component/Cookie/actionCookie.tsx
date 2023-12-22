e26eb7eabecfdabc5bfc7ca8b7aeb48546b4464f
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-22 12:45:51 +0700

    Updated cookies - set v1

--- src/component/Cookie/actionCookie.tsx
+++ src/component/Cookie/actionCookie.tsx
@@ -3,28 +3,32 @@ import { cookies } from "next/headers";
 
 const setCookie = async (user:any) => {
     cookies().set({
-        name: 'vocal-auth-google',
+        name: 'vocal-auth-google-cl',
         value: user,
         secure: true,
         httpOnly: true,
         path: '/',
         domain: process.env.BE_URL_DOMAIN,
-        sameSite: 'none', 
+        sameSite: 'none',
         maxAge: 60 * 60 * 1000
     })
+
     cookies().set({
-        name: 'vocal-auth-google',
+        name: 'vocal-auth-google-sv',
         value: user,
         secure: true,
         httpOnly: true,
         path: '/',
-        domain: 'nguyenhaikendy-api-be-vocal.onrender.com',
-        sameSite: 'none', 
+        domain: process.env.BE_URL_DOMAIN,
+        sameSite: 'lax', 
         maxAge: 60 * 60 * 1000
     })
 }
 
-const removeCookie = () => cookies().delete('vocal-auth-google')
+const removeCookie = () => {
+    cookies().delete('vocal-auth-google-cl')
+    cookies().delete('vocal-auth-google-sv')
+}
 
 export { setCookie, removeCookie }
 
