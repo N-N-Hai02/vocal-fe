95e29a09a21ae556d176671e5c042139a47d27ba
nguyen Ngoc hai <nguyenngochai1682002@gmail.com>
2023-12-22 15:11:37 +0700

    Update - actionCookies v7

--- src/component/Cookie/actionCookie.tsx
+++ src/component/Cookie/actionCookie.tsx
@@ -2,32 +2,32 @@
 import { cookies } from "next/headers";
 
 const setCookie = async (user:any) => {
-    cookies().set({
-        name: 'vocal-auth-google-cl',
-        value: user,
-        secure: true,
-        httpOnly: true,
-        path: '/',
-        domain: "nguyenngochai-vocalbulary-english-v3.vercel.app",
-        sameSite: 'none',
-        maxAge: 60 * 60 * 1000
-    })
+    // cookies().set({
+    //     name: 'vocal-auth-google-cl',
+    //     value: user,
+    //     secure: true,
+    //     httpOnly: true,
+    //     path: '/',
+    //     domain: 'localhost', // "nguyenngochai-vocalbulary-english-v3.vercel.app",
+    //     sameSite: 'none',
+    //     maxAge: 60 * 60 * 1000
+    // })
 
-    cookies().set({
-        name: 'vocal-auth-google-sv',
-        value: user,
-        secure: true,
-        httpOnly: true,
-        path: '/',
-        domain: "nguyenhaikendy-api-be-vocal.onrender.com",
-        sameSite: 'none', 
-        maxAge: 60 * 60 * 1000
-    })
+    // cookies().set({
+    //     name: 'vocal-auth-google-sv',
+    //     value: user,
+    //     secure: true,
+    //     httpOnly: true,
+    //     path: '/',
+    //     domain: 'localhost', // "nguyenhaikendy-api-be-vocal.onrender.com",
+    //     sameSite: 'none', 
+    //     maxAge: 60 * 60 * 1000
+    // })
 }
 
 const removeCookie = () => {
-    cookies().delete('vocal-auth-google-cl')
-    cookies().delete('vocal-auth-google-sv')
+    // cookies().delete('vocal-auth-google-cl')
+    // cookies().delete('vocal-auth-google-sv')
 }
 
 export { setCookie, removeCookie }
